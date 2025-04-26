79d756d49dadfdfc4c685747525a32f3ddc5b245
Cha Krupka <chakrupka238@gmail.com>
2025-04-26 19:16:49 -0400

    initial app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function App() {
+  return (
+    <div className="size-full items-center bg-gradient-to-b from-cyan-100 to-blue-200">
+      Hello
+    </div>
+  );
+}
+
+export default App;
