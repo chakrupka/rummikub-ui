c5f2cff626ad479366d7ac16f42becf67c15635c
Cha Krupka <107597448+chakrupka@users.noreply.github.com>
2025-05-06 00:17:27 -0400

    fix view name

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -64,7 +64,7 @@ export default function Home() {
             )}
             onClick={() => setViewingSets(false)}
           >
-            Tiles
+            Rack
           </button>
         </div>
       </div>
